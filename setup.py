06994ac7f8dbaa260811e6c797b364711e3e79d4
DrArtemi <adrien.milcent@outlook.com>
2021-10-23 20:47:58 +0200

    housing prices competition tests with xgboost

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='kaggle_experiments',
+    version='1.0',
+    description='Kaggle experiments',
+    author='Adrien Milcent',
+    author_email='adrien.milcent@outlook.com',
+)
